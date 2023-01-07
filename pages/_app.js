59805f958f523e7eec5c0c98018abf9e6733272f
David Fales <davidfalesct@gmail.com>
2023-01-07 14:35:28 -0700

    added session provider

---
+++ pages/_app.js
@@ -0,0 +1,23 @@
+import '../styles/globals.css'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider, useUser } from '@supabase/auth-helpers-react'
+import { useState } from 'react'
+import { AppProps } from 'next/app'
+import { NextUIProvider } from "@nextui-org/react"
+
+function MyApp({ Component, pageProps }) {
+  const [supabase] = useState(() => createBrowserSupabaseClient())
+  const user = useUser()
+
+  return (
+    <SessionContextProvider 
+    supabaseClient={supabase}
+    >
+      <NextUIProvider>
+      <Component {...pageProps} />
+      </NextUIProvider>
+    </SessionContextProvider>
+
+  )
+}
+export default MyApp
