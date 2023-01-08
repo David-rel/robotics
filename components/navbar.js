61d8cec5792baa02cd3dc5444cbf5321e093e661
David Fales <davidfalesct@gmail.com>
2023-01-08 15:05:26 -0700

    added front end for login

--- components/navbar.js
+++ components/navbar.js
@@ -6,14 +6,14 @@ function navbar() {
 
 <nav class="bg-white border-gray-200 dark:bg-gray-900">
     <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
-        <a href="https://flowbite.com" class="flex items-center">
+        <a href="https://www.regisjesuit.com/" class="flex items-center">
             <img src="https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/304042949_747567189980158_3603668779920286313_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=qtSOORBo3PUAX_mqYQb&_nc_ht=scontent-lga3-1.xx&oh=00_AfAL3ljOk2NuBosGBAnl8RRQNlH0nf8Uw-mlzcWSOA5S0g&oe=63C066BA" class="h-6 mr-3 sm:h-9" alt="RJ logo" />
             <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">RJ Robotics</span>
         </a>
         <div class="flex items-center">
-            <a href="tel:5541251234" class="mr-6 text-sm font-medium text-gray-500 dark:text-white hover:underline">6300 S Lewiston Way, Aurora, CO 80016
+            <a href="https://www.google.com/maps/place/6300+S+Lewiston+Way,+Aurora,+CO+80016/@39.6011631,-104.8005882,17z/data=!3m1!4b1!4m5!3m4!1s0x876c88d4a3602fc3:0x4e5549fd92d581c!8m2!3d39.601159!4d-104.7983995" class="mr-6 text-sm font-medium text-gray-500 dark:text-white hover:underline">6300 S Lewiston Way, Aurora, CO 80016
 </a>
-            <a href="#" class="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline">Login</a>
+            <a href="/Login" class="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline">Login</a>
         </div>
     </div>
 </nav>
@@ -22,7 +22,9 @@ function navbar() {
         <div class="flex items-center">
             <ul class="flex flex-row mt-0 mr-6 space-x-10 text-sm font-medium">
                 <li>
-                    <a href="/" class="text-gray-900 dark:text-white hover:underline" aria-current="page">Home</a>
+                    <a href="/" class="text-gray-900 dark:text-white hover:underline"
+                    //  aria-current="page" 
+                     >Home</a>
                 </li>
                 <li>
                     <a href="/About" class="text-gray-900 dark:text-white hover:underline">About</a>
---
+++ pages/Login/index.js
@@ -0,0 +1,65 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <section class="h-screen">
+  <div class="px-6 h-full text-gray-800">
+    <div
+      class="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6"
+    >
+      <div
+        class="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0"
+      >
+        <img
+          src="https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/304042949_747567189980158_3603668779920286313_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=qtSOORBo3PUAX_mqYQb&_nc_ht=scontent-lga3-1.xx&oh=00_AfAL3ljOk2NuBosGBAnl8RRQNlH0nf8Uw-mlzcWSOA5S0g&oe=63C066BA"
+          class="w-full"
+          alt="Sample image"
+        />
+      </div>
+      <div class="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
+        <form>
+
+          <div class="mb-6">
+            <input
+              type="text"
+              class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              id="exampleFormControlInput2"
+              placeholder="Email address"
+            />
+          </div>
+
+          <div class="mb-6">
+            <input
+              type="password"
+              class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              id="exampleFormControlInput2"
+              placeholder="Password"
+            />
+          </div>
+
+
+          <div class="text-center lg:text-left">
+            <button
+              type="button"
+              class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+            >
+              Login
+            </button>
+          </div>
+          <p class="text-sm font-semibold mt-2 pt-1 mb-0">
+              Don't have an account?
+              <a
+                href="/Signup"
+                class="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
+                >Signup</a
+              >
+            </p>
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
+  )
+}
+
+export default Login
